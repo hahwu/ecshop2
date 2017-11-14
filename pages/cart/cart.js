748ca64d84290d29b88d327919aaedad31928241
hahwu <31872165+hahwu@users.noreply.github.com>
2017-11-14 12:00:41 +0800

    1

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -8,6 +8,9 @@ Page({
     showFoot:true,
     showTop:false,
     text:'编辑',
+    total_price:0,
+    total_num:0,
+    margin:120,
   },
   /**
    * 生命周期函数--监听页面加载
@@ -21,10 +24,10 @@ Page({
         user_id:wx.getStorageSync('user_id'),
       },
       success:function(res){
-        console.log(res.data)
         that.setData({
           carts:res.data
         })
+        that.refresh()
       }
     })
   },
@@ -82,30 +85,34 @@ Page({
       this.setData({
         showTop: true,
         showFoot: false,
-        text: '完成'
+        text: '完成',
+        margin:220
       })
     }else{
       this.setData({
         showTop: false,
         showFoot: true,
-        text: '编辑'
+        text: '编辑',
+        margin:120,
       })
     }
   },
   subtraction:function(res){
     var index = res.target.dataset.num
-    var goods_num = this.data.carts[index]['goods_number']
-    if(goods_num <2) return false
+    var goods_num = parseInt(this.data.carts[index]['goods_number']) - 1
+    if(goods_num < 1) return false
     var that = this
     wx.request({
       url: 'https://www.yuncms.online/tomato/wx_cart.php',
       data:{
         mode:'subtraction',
         rec_id:res.currentTarget.id,
+        goods_number:goods_num
       },
       success:function(res){
         var carts = that.data.carts
-        carts[index]['goods_number'] = res.data
+        carts[index]['goods_number'] = goods_num
+        that.refresh()
         that.setData({
           carts:carts
         })
@@ -114,22 +121,39 @@ Page({
   },
   add:function(res){
     var index = res.target.dataset.num
-    var goods_num = this.data.carts[index]['goods_number']
-    if (goods_num < 2) return false
+    var goods_num = parseInt(this.data.carts[index]['goods_number']) + 1
+    if (goods_num >99) return false
     var that = this
     wx.request({
       url: 'https://www.yuncms.online/tomato/wx_cart.php',
       data: {
         mode: 'add',
         rec_id: res.currentTarget.id,
+        goods_number:goods_num
       },
       success: function (res) {
         var carts = that.data.carts
-       console.log(carts[index])
+        carts[index]['goods_number'] = parseInt(goods_num)
+        that.refresh()
         that.setData({
           carts: carts
         })
       }
+    })
+  },
+  refresh:function(res){
+    var total_price = 0
+    var total_num = 0
+    var cart = this.data.carts
+    console.log(cart)
+    for (var i = 0; i < cart.length;i++){
+      total_price+= parseFloat(cart[i]['goods_price'])*parseInt(cart[i]['goods_number'])
+      total_num+=parseInt(cart[0]['goods_number'])
+      console.log(total_price)
+    }
+    this.setData({
+      total_price:total_price,
+      total_num:total_num
     })
   }
 })
