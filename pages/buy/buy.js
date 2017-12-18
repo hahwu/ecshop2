e252bf6d1c86730418ff293ae8313c410efde9ba
hahwu <31872165+hahwu@users.noreply.github.com>
2017-12-18 18:03:00 +0800

    111

--- pages/buy/buy.js
+++ pages/buy/buy.js
@@ -26,53 +26,29 @@ Page({
     })
     console.log(options)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  /*
+  **选择新地址
+  */
+  chooseAdd: function (res) {
+    var that = this
+    wx.chooseAddress({
+      success: function (res) {
+        console.log(res)
+        wx.request({
+          url: 'https://www.yuncms.online/tomato/wx_user.php',
+          data: {
+            mode: 'addAddress',
+            address: res,
+            user_id: wx.getStorageSync('user_id')
+          },
+          success: function (res) {
+            console.log(res.data)
+          }
+        })
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
